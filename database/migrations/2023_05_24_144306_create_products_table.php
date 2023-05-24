c4abdbdcd5ab0dd1c327ad7eb54fd2ec98b19b9f
arifulislamrana <ariful.islam0683@gmail.com>
2023-05-24 20:58:04 +0600

    up to products migration

---
+++ database/migrations/2023_05_24_144306_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('price');
+            $table->string('description');
+            $table->string('image');
+            $table->string('status')->default('active');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function(Blueprint $table){
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
+        Schema::dropIfExists('products');
+    }
+};
